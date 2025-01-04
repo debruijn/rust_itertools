d35695e1164c20517b77ec85a62088cc0b64c456
Bert de Bruijn <lpdebruijn@gmail.com>
2025-01-03 20:18:13 -0400

    Apply mypy tips

--- python/rust_itertools/__init__.py
+++ python/rust_itertools/__init__.py
@@ -1,3 +1,6 @@
+from collections.abc import Sized
+from typing import Any
+
 from . import _rust_itertools
 
 __all__ = (
@@ -18,16 +21,16 @@ VERSION = _rust_itertools.VERSION
 derangements_range = _rust_itertools.derangements_range
 
 
-def derangements(x, k=None):
-    return _rust_itertools.derangements(x, len(x) if k is None else k)
+def derangements(iterable: Sized, k: int | None) -> list[list[Any]]:
+    return _rust_itertools.derangements(iterable, len(iterable) if k is None else k)
 
 
-def permutations(x, k=None):
-    return _rust_itertools.permutations(x, len(x) if k is None else k)
+def permutations(iterable: Sized, k: int | None) -> list[list[Any]]:
+    return _rust_itertools.permutations(iterable, len(iterable) if k is None else k)
 
 
-def distinct_permutations(x, k=None):
-    return _rust_itertools.distinct_permutations(x, len(x) if k is None else k)
+def distinct_permutations(iterable: Sized, k: int | None) -> list[list[Any]]:
+    return _rust_itertools.distinct_permutations(iterable, len(iterable) if k is None else k)
 
 
 combinations = _rust_itertools.combinations
